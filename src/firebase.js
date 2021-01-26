d2b712e4c3bec4db8465913cfdb78fec59c38d9b
jplogesh <jplogesh>
2021-01-26 15:43:44 +0530

    Initialize whatsapp clone with basic functions

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import Firebase from "./firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBRQAREJK1FQNYNnJxVJ2H9TXAn6fHtJuU",
+    authDomain: "whatsapp-clone-6788f.firebaseapp.com",
+    projectId: "whatsapp-clone-6788f",
+    storageBucket: "whatsapp-clone-6788f.appspot.com",
+    messagingSenderId: "657623994402",
+    appId: "1:657623994402:web:bc3e2990cebf3513a27a7b",
+    measurementId: "G-PBB0BCR4QT"
+  };
